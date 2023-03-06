523f133a762a06ebfd2bd0c914a5ceeebbad2cde
Ferdaws Kukcha <ferdaws_kukcha@yahoo.com>
2023-03-06 17:19:45 +0100

    Added some dummy movie data in initializeMovies method.

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -9,7 +9,7 @@ public class Movie {
     // TODO add more properties here
 
     //genre List
-    private List<String> genres;
+    private List<Genre> genres = List.of(Genre.values());
 
     // Front Cover IMG Object?
 
@@ -29,7 +29,18 @@ public class Movie {
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
         // TODO add some dummy data here
-
+        movies.add(new Movie(
+                "The Matrix",
+                "The Matrix is a computer-generated dream world designed to keep these humans under control. Humans are kept sedated, effectively living a virtual life. Neo awakens in a bed back on Morpheus's ship, and Morpheus further explains that one man was born into the Matrix with the power to change anything in it."
+        ));
+        movies.add(new Movie(
+                "Avatar",
+                "To explore Pandora, genetically matched human scientists use Na'vi-human hybrids called \"avatars.\" Paraplegic Marine Jake Sully is sent to Pandora to replace his deceased identical twin, who had signed up to be an operator."
+        ));
+        movies.add(new Movie(
+                "The Dark Knight Rises",
+                "Eight years after the Joker's reign of chaos, Batman is coerced out of exile with the assistance of the mysterious Selina Kyle in order to defend Gotham City from the vicious guerrilla terrorist Bane"
+        ));
         return movies;
     }
 }
