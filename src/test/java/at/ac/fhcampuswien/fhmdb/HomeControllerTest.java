6a279946e72290af08b35aff47f0b7472ed09c42
Ferdaws Kukcha <ferdaws_kukcha@yahoo.com>
2023-03-09 01:48:40 +0100

    Initial commit.

---
+++ src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -0,0 +1,12 @@
+package at.ac.fhcampuswien.fhmdb;
+
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+}
