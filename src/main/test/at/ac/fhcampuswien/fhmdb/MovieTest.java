de22b1fe911669547484d0db9d61c2cee48ba536
fkukcha <ferdaws_kukcha@yahoo.com>
2023-04-03 00:35:40 +0200

    Moved the MovieTest class into another package.

--- src/main/test/at/ac/fhcampuswien/fhmdb/MovieTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/MovieTest.java
@@ -1,5 +1,7 @@
-package at.ac.fhcampuswien.fhmdb.models;
+package at.ac.fhcampuswien.fhmdb;
 
+import at.ac.fhcampuswien.fhmdb.models.Genre;
+import at.ac.fhcampuswien.fhmdb.models.Movie;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
