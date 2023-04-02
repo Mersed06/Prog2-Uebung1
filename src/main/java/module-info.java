acf160de50bf1cc2584a3dbdfdd7472f6cf6d690
fkukcha <ferdaws_kukcha@yahoo.com>
2023-04-03 00:34:51 +0200

    Added okhttp3 requirements.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module at.ac.fhcampuswien.fhmdb {
 
     requires com.jfoenix;
 
+    requires okhttp3;
+
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
     exports at.ac.fhcampuswien.fhmdb;
 }
